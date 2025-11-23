7c9565888989beac69db4760193cf73178c6258d
Alexey Utkin <cedrickobayashi@gmail.com>
2025-11-23 18:25:24 +0300

    Test class was completed - build failed

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assert.assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void shouldCalculateFromEqual() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assert.assertEquals(service.remain(1000), 0);
+    }
+
+    @Test
+    public void shouldCalculateFromBoundaryVolume() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assert.assertEquals(service.remain(1001), 999);
+    }
+
+    @Test
+    public void shouldCalculateFromZero() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assert.assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldCalculateFromNegative() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assert.assertEquals(service.remain(-1), 1001);
+    }
+}

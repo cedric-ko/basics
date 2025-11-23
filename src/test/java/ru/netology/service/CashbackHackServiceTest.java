baa142fe10f80205e509d4ead65627bf3adc605b
Alexey Utkin <cedrickobayashi@gmail.com>
2025-11-23 21:08:36 +0300

    Task 2 is done

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.service;
 import org.junit.Test;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
@@ -10,34 +11,69 @@ public class CashbackHackServiceTest {
     public void shouldCalculateRemain() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals(1, service.remain(999));
+    }
+
+    @Test
+    public void shouldCalculateRemain_inJUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(1, service.remain(999));
     }
 
     @Test
     public void shouldCalculateFromEqual() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @Test
+    public void shouldCalculateFromEqual_inJUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(0, service.remain(1000));
     }
 
     @Test
     public void shouldCalculateFromBoundaryVolume() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(1001), 999);
+        Assert.assertEquals(999, service.remain(1001));
+    }
+
+    @Test
+    public void shouldCalculateFromBoundaryVolume_inJUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(999, service.remain(1001));
     }
 
     @Test
     public void shouldCalculateFromZero() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(1000, service.remain(0));
+    }
+
+    @Test
+    public void shouldCalculateFromZero_inJUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(1000, service.remain(0));
     }
 
     @Test
     public void shouldCalculateFromNegative() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(-1), 1001);
+        Assert.assertEquals(1001, service.remain(-1));
+    }
+
+    @Test
+    public void shouldCalculateFromNegative_inJUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(1001, service.remain(-1));
     }
 }

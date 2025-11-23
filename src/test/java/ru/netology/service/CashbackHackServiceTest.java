fb0a284f0f09cf0581e93adff7c4467c11db479d
Alexey Utkin <cedrickobayashi@gmail.com>
2025-11-23 20:33:10 +0300

    JUnit4 tests were fixed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,34 +10,34 @@ public class CashbackHackServiceTest {
     public void shouldCalculateRemain() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals(1, service.remain(999));
     }
 
     @Test
     public void shouldCalculateFromEqual() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(0, service.remain(1000));
     }
 
     @Test
     public void shouldCalculateFromBoundaryVolume() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(1001), 999);
+        Assert.assertEquals(999, service.remain(1001));
     }
 
     @Test
     public void shouldCalculateFromZero() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(1000, service.remain(0));
     }
 
     @Test
     public void shouldCalculateFromNegative() {
         CashbackHackService service = new CashbackHackService();
 
-        Assert.assertEquals(service.remain(-1), 1001);
+        Assert.assertEquals(1001, service.remain(-1));
     }
 }
